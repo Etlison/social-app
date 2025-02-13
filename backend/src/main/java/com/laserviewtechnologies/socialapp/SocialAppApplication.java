9972fe023b45ac3e5a9a859d5842f846ce0882fd
Etien <etienmbah@gmail.com>
2025-02-13 03:29:53 +0000

    Modified pom.xml file to cobert .jar to .war for tomcat deployment

--- backend/src/main/java/com/laserviewtechnologies/socialapp/SocialAppApplication.java
+++ backend/src/main/java/com/laserviewtechnologies/socialapp/SocialAppApplication.java
@@ -2,13 +2,19 @@ package com.laserviewtechnologies.socialapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialAppApplication {
+public class SocialAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        // Launch the Spring Boot application
         SpringApplication.run(SocialAppApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SocialAppApplication.class);
     }
 }
 
